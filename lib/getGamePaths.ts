e5f3d162c9a1bc1a4242fa5a609754e408f6420a
Fayne Aldan <FayneAldan@gmail.com>
2021-10-26 14:46:15 -0600

    Seperate game path calculations into lib

---
+++ lib/getGamePaths.ts
@@ -0,0 +1,48 @@
+import { parse, path } from "../deps.ts";
+
+export interface GamePaths {
+  SkyrimSE?: string;
+  Fallout4?: string;
+}
+
+const args = parse(Deno.args, {
+  string: ["lib", "sse", "fo4"],
+});
+
+const lib: string = args.lib || path.join("C:", "Program Files (x86)", "Steam");
+const common = path.join(lib, "steamapps", "common");
+
+const ssePath = args.sse || path.join(common, "Skyrim Special Edition");
+const fo4Path = args.fo4 || path.join(common, "Fallout 4");
+
+const handleDef = !args.lib && !args.sse && !args.fo4;
+if (handleDef) {
+  console.log("No arguments provided. Assuming default install locations.");
+}
+
+const handleLib = !!(args.lib) || handleDef;
+const handleSSE = handleLib || !!(args.sse);
+const handleFO4 = handleLib || !!(args.fo4);
+
+export async function checkReady(gamePath: string) {
+  let status = await Deno.permissions.request({ name: "read", path: gamePath });
+  if (status.state !== "granted") {
+    throw "Permission denied. Skipping...";
+  }
+  try {
+    await (Deno.stat(gamePath));
+  } catch {
+    throw "Failed to find game. Skipping...";
+  }
+}
+
+export default function getGamePaths(): GamePaths {
+  let ret: GamePaths = {};
+  if (handleSSE) {
+    ret.SkyrimSE = ssePath;
+  }
+  if (handleFO4) {
+    ret.Fallout4 = fo4Path;
+  }
+  return ret;
+}
--- update.ts
+++ update.ts
@@ -1,51 +1,28 @@
 import { update } from "./lib/update.ts";
-import { grantOrThrow, parse, path } from "./deps.ts";
+import { path } from "./deps.ts";
+import getGamePaths, { checkReady } from "./lib/getGamePaths.ts";
 
-const args = parse(Deno.args, {
-  string: ["lib", "sse", "fo4"],
-});
-let lib: string = args.lib || path.join("C:", "Program Files (x86)", "Steam");
-let common = path.join(lib, "steamapps", "common");
-let ssePath = args.sse || path.join(common, "Skyrim Special Edition");
-let fo4Path = args.fo4 || path.join(common, "Fallout 4");
+const paths = getGamePaths();
 
-const handleDef = !args.lib && !args.sse && !args.fo4;
-if (handleDef) {
-  console.log("No arguments provided. Assuming default install locations.");
-}
-
-const handleLib = !!(args.lib) || handleDef;
-const handleSSE = handleLib || !!(args.sse);
-const handleFO4 = handleLib || !!(args.fo4);
-
-async function isReady(gamePath: string): Promise<boolean> {
-  const maniPath = path.join(gamePath, "Creations");
-  let status = await Deno.permissions.request({ name: "read", path: maniPath });
-  if (status.state !== "granted") {
-    console.log("Permission denied. Skipping...");
-    return false;
-  }
-  try {
-    await (Deno.stat(maniPath));
-    console.log("Game found. Starting update...");
-    return true;
-  } catch {
-    console.log("Failed to find game. Skipping...");
-    return false;
-  }
-}
-
-if (handleSSE) {
+if (paths.SkyrimSE) {
   console.log("Checking for Skyrim Special Edition...");
-  if (await isReady(ssePath)) {
-    await update(ssePath, "skyrimse.json");
+  try {
+    await checkReady(path.join(paths.SkyrimSE, "Creations"));
+    console.log("Game found. Starting update...");
+    await update(paths.SkyrimSE, "skyrimse.json");
+  } catch (e) {
+    console.error(e);
   }
 }
 
-if (handleFO4) {
+if (paths.Fallout4) {
   console.log("Checking for Fallout 4...");
-  if (await isReady(fo4Path)) {
-    await update(fo4Path, "fallout4.json");
+  try {
+    await checkReady(path.join(paths.Fallout4, "Creations"));
+    console.log("Game found. Starting update...");
+    await update(paths.Fallout4, "fallout4.json");
+  } catch (e) {
+    console.error(e);
   }
 }
 
